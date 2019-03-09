49ba88d525c4b7744e13c4b5789145766367879d
Bright Mazura <mazurabk@gmail.com>
2019-03-08 19:07:01 -0700

    Completed Project

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+Code by: Kallas Mazura
+
+*/
+let API_URI = "https://api.iextrading.com/1.0";
+
+let app = new Vue({
+  el: "#app",
+
+  data: {
+    loading: true,
+    errored: false,
+    tickerSymbol: "",
+    stockData: {}
+  },
+
+  created: function() {
+    this.getStockData();
+  },
+
+  methods: {
+    getStockData: function() {
+      let self = this;
+
+      axios
+        .get(
+          API_URI + "/stock/aapl/batch?types=quote,news,chart&range=1m&last=10"
+        )
+        .then(response => {
+          this.stockData = response.data.quote;
+          this.loading = false;
+        })
+        .catch(err => {
+          console.error(err);
+          errored = true;
+        });
+    }
+  }
+});
